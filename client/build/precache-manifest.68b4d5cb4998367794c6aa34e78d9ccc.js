f8ec4d15ea96825fdaeca4e2f920a04bb37b535c
rohit163 <rohitphatarpekar@gmail.com>
2020-07-11 03:39:31 +0530

    stripe

---
+++ client/build/precache-manifest.68b4d5cb4998367794c6aa34e78d9ccc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f78bbe7aabc0d8da1d7d40011fbba9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13cab1ba939bf1c6ff33",
+    "url": "/static/css/main.3dcfda9c.chunk.css"
+  },
+  {
+    "revision": "8720a72d1d075776ce9a",
+    "url": "/static/js/2.d8de162e.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.d8de162e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cab1ba939bf1c6ff33",
+    "url": "/static/js/main.2de31389.chunk.js"
+  },
+  {
+    "revision": "ce3de9bd58211d2ec8c1",
+    "url": "/static/js/runtime-main.4330aa0b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+// import { response } from 'express';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51GzjJZJMQfpoppFEVVy0REha6SQUCUIjN1OflEz1SXgpYDsG9ISu0Zi7EPuovrFGNCZ07VbR2RWmJ0DCf9OXjCWR00ozL09mtL';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
